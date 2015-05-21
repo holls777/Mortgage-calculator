9a68b30f468863332c01af2eac989c4b22dcfe68
holls777 <holliday777@verizon.net>
2015-05-20 23:40:11 -0400

    a

--- src/main/main.java
+++ src/main/main.java
@@ -1,17 +1,17 @@
 package main;
 
 public class main {
-double totalgrossincome;
-double totalmonthlydebt;
-double mortgageinterestrate;
-double term;
-double downpayment;
+public double totalgrossincome;
+public double totalmonthlydebt;
+public double mortgageinterestrate;
+public double term;
+//double downpayment;
      public double HousingPaymentOnly(double totalgrossincome){
     	 double HousingPaymentalone=(totalgrossincome/12)*.18;
     	 return HousingPaymentalone;
      }
-     public double HousingPaymentOther(double totalgrossincome){
-    	 double HousingPaymentAdd= (totalgrossincome/12)*.36;
+     public double HousingPaymentOther(double totalgrossincome, double totalmonthlydebt){
+    	 double HousingPaymentAdd= (totalgrossincome/12)*.36-totalmonthlydebt;
     	 return HousingPaymentAdd;
      }
      public double MaxPay(double HousingPaymentalone,double HousingPaymentAdd){
@@ -24,5 +24,13 @@ double downpayment;
     	 else;
     	 return 0;
      }
-     
+     public double MortgageEstimate(double totalgrossincome, double totalmonthlydebt, double mortgageinterestrate, int term){
+    	 double a=HousingPaymentOnly(totalgrossincome);
+    	 double b=HousingPaymentOther(totalgrossincome, totalmonthlydebt);
+    	 double c=MaxPay(a,b);
+    	 term=30;//fix before submit
+    	 int sub1=(int) (1.0+mortgageinterestrate);
+    	 double capablefinance=((sub1^(term*12)-1)/mortgageinterestrate(1+mortgageinterestrate)^(term*12));
+    	 return capablefinance;
+     }
 }
